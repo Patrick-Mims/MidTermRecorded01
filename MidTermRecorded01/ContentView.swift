6d407545b782b3733b3dfeda88afcc962aa85c32
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-28 18:44:47 -0700

    Initial Commit

---
+++ MidTermRecorded01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MidTermRecorded01
+//
+//  Created by JPL-ST-SPRING2022 on 10/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MidTermRecorded01/MidTermRecorded01App.swift
@@ -0,0 +1,17 @@
+//
+//  MidTermRecorded01App.swift
+//  MidTermRecorded01
+//
+//  Created by JPL-ST-SPRING2022 on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MidTermRecorded01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
